a3dd5f880a9a65739ccf81b4f06ba66bdb27bc84
Jules <dali1999@naver.com>
2024-02-27 22:30:04 +0900

    feat: Dropdown component 추가

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import * as S from '@Components/Dropdown.style';
+
+const OPTION_LISTS = [
+  {
+    title: '친구',
+  },
+  {
+    title: '지인',
+  },
+  {
+    title: '동료',
+  },
+  {
+    title: '가족',
+  },
+];
+
+function Dropdown({ disabled }) {
+  const [isdrop, setIsdrop] = useState(false);
+  const [selected, setSelected] = useState(OPTION_LISTS[0]);
+
+  const handleDropdownClick = () => {
+    setIsdrop(!isdrop);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelected(option);
+    setIsdrop(false);
+  };
+
+  return (
+    <S.Dropdown onClick={handleDropdownClick}>
+      <S.DropdownElement placeholder="Placeholder" disabled={disabled} $isdrop={isdrop}>
+        <S.Title disabled={disabled}>{selected.title}</S.Title>
+        <S.ArrowIcon src={isdrop ? '/images/icons/down-arrow.svg' : '/images/icons/up-arrow.svg'} alt="" />
+      </S.DropdownElement>
+      {isdrop && (
+        <S.Options>
+          {OPTION_LISTS.map((list) => (
+            <S.Option key={list.title} onClick={() => handleOptionClick(list)}>
+              {list.title}
+            </S.Option>
+          ))}
+        </S.Options>
+      )}
+    </S.Dropdown>
+  );
+}
+
+export default Dropdown;
--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Input from '@Components/Input';
+import Dropdown from '@Components/Dropdown';
 
 function Test() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Input disabled />
+      <Input />
+      <Input error />
+      <Dropdown disabled />
+      <Dropdown />
+    </div>
+  );
 }
 
 export default Test;
