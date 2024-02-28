abf291cdfc104405ed8b870f843472cc856d8a2a
parkhyeonwoo <pq12owal@naver.com>
2024-02-28 14:27:44 +0900

    feat : create PrimaryDeleteBtn component

--- src/components/ui/PrimaryComponent.style.js
+++ src/components/ui/PrimaryComponent.style.js
@@ -8,6 +8,7 @@ const Primary = css`
   display: inline-flex;
   justify-content: center;
   align-items: center;
+  outline: none;
   &:focus {
     border: 2px solid var(--primary900);
     background: var(--primary800);
@@ -18,9 +19,10 @@ export const PrimaryLink = styled(Link)`
   ${Primary}
   width: 280px;
   height: 56px;
-  padding: 14px 24px;
   font-size: 18px;
   border-radius: 12px;
+  margin-left: auto;
+  margin-right: auto;
   @media ${device.tablet} {
     width: 100%;
   }
@@ -30,10 +32,8 @@ export const PrimaryCreateBtn = styled.button`
   ${Primary}
   width : 100%;
   height: 56px;
-  padding: 14px 24px;
   font-size: 18px;
   border-radius: 12px;
-  outline: none;
   &:active {
     background: var(--primary800);
   }
@@ -48,9 +48,35 @@ export const PrimaryCardModalBtn = styled.button`
   height: 40px;
   font-size: 16px;
   border-radius: 6px;
-  outline: none;
   margin: 31px auto 0;
+  &:active {
+    background: var(--primary800);
+  }
   @media ${device.tablet} {
     margin: 24px auto 0;
+  }
+`;
+
+export const PrimaryDeleteBtn = styled.button`
+  ${Primary}
+  width : 92px;
+  height: 39px;
+  font-size: 16px;
+  border-radius: 6px;
+  outline: none;
+  &:active {
+    background: var(--primary800);
+  }
+  @media ${device.tablet} {
+    width: calc(100% - 48px);
+    height: 56px;
+    border-radius: 12px;
+    font-size: 18px;
+    font-weight: 700;
+    position: fixed;
+    bottom: 24px;
+    left: 50%;
+    z-index: 1000;
+    transform: translateX(-50%);
   }
 `;
