ee9a9a903a6525fb90f3dd69f8cf691895d94cc4
d5ng <dong5ffice@gmail.com>
2024-02-29 01:56:55 +0900

    Feat: 롤링페이퍼 페이지 작업중.

---
+++ src/pages/rolling/paper/Paper.style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const colors = {
+  green: 'var(--green200)',
+};
+
+export const Section = styled.section`
+  background-color: ${({ $bgColor }) => colors[$bgColor]};
+`;
+
+export const PaperWrapper = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 113px 0 246px 0;
+
+  display: grid;
+  grid-template-columns: repeat(3, 384px);
+  gap: 24px;
+`;
+
+export const PostButton = styled.button`
+  height: 280px;
+  background-color: var(--white);
+  border-radius: 16px;
+`;
