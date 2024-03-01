51b382963ae457186fc0524857404a973c483335
Jules <dali1999@naver.com>
2024-03-01 18:35:23 +0900

    feat: 배경 이미지 선택 모달 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import * as Page from 'pages';
+import ImagePickerModal from 'pages/form/components/ImagePickerModal';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Page.Root />}>
           <Route index element={<Page.Home />} />
+          <Route path="picker" element={<ImagePickerModal />} />
           <Route path="post" element={<Page.PostCreate />} />
           <Route path="list" element={<Page.RollingList />} />
           <Route path="post/:recipientsId" element={<Page.Paper />} />
