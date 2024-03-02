eb5006cd8b1d299ebdb367635fbcda71645fec90
d5ng <dong5ffice@gmail.com>
2024-03-02 12:32:17 +0900

    PostCreate Add

--- src/pages/rolling/paper/Paper.jsx
+++ src/pages/rolling/paper/Paper.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { PaperContextProvider } from '@Paper/context/PaperContext';
+import PostCreate from 'pages/post/PostCreate';
 import PaperContents from './components/PaperContents';
 
 function Paper() {
   return (
     <PaperContextProvider>
-      {/* <PostCreate /> */}
+      <PostCreate />
       <PaperContents />
     </PaperContextProvider>
   );
