f748797763def42c836c9b3ce9acbcab8db275b5
Jules <dali1999@naver.com>
2024-03-04 12:32:09 +0900

    feat: Paper create 페이지에서 이미 존재하는 이름 처리하는 기능 추가

--- src/pages/form/context/FormContext.jsx
+++ src/pages/form/context/FormContext.jsx
@@ -1,16 +1,26 @@
 import React, { createContext, useContext, useEffect, useMemo, useState } from 'react';
-import { getBackgroundImages } from '../api';
+import { getAllPapersInfo, getBackgroundImages } from '../api';
 
 const FormContext = createContext();
 
 export const useFormContext = () => useContext(FormContext);
 
 export function FormProvider({ children }) {
+  const [papersInfo, setPapersInfo] = useState([]);
+
   const [selectedBtn, setSelectedBtn] = useState('color');
   const [selectedBackground, setSelectedBackground] = useState('');
   const [backgroundImages, setBackgroundImages] = useState([]);
   const [unsplashBackgroundImages, setUnsplashBackgroundImages] = useState([]);
   const [searchedImages, setSearchedImages] = useState([]);
+
+  // 전체 Paper 로드 함수(중복 확인용)
+  const handleLoadPapersInfo = async () => {
+    const data = await getAllPapersInfo();
+    if (!data) return;
+    const papers = data.results.map((paper) => paper.name);
+    setPapersInfo(papers);
+  };
 
   // 배경 이미지 로드 함수
   const handleLoadImages = async () => {
@@ -54,6 +64,9 @@ export function FormProvider({ children }) {
 
   // eslint-disable-next-line react/jsx-no-constructed-context-values
   const values = {
+    papersInfo,
+    handleLoadPapersInfo,
+
     handleLoadImages,
 
     selectedBtn,
