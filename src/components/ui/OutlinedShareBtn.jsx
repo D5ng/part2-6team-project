26aab50d1a93daac2e6b6f6acbc4560955b3aed2
GoM <honorrulru@gmail.com>
2024-03-04 16:20:50 +0900

    feat: Toast.style 조금 수정, KaKaoShareButton 컴포넌트 분리, URL공유 복사 기능 추가

--- src/components/ui/OutlinedShareBtn.jsx
+++ src/components/ui/OutlinedShareBtn.jsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
 import * as S from '@Components/ui/OutlinedComponent.style';
+import copy from 'clipboard-copy';
+import Toast from './Toast';
+import KakaoShareButton from '../../pages/post/components/KakaoShareButton';
 
-function OutlinedShareBtn({ kakaoApi, cloneURL }) {
+function OutlinedShareBtn() {
   const [hidden, setHidden] = useState(true);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = () => {
+    const currentUrl = window.location.href;
+
+    copy(currentUrl);
+
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 5000);
+  };
   return (
-    <S.ShareBtn onClick={() => setHidden(!hidden)}>
-      <S.ButtonIconImg src="/images/icons/shareIcon.svg" alt="공유버튼 아이콘" />
-      <S.Wrap hidden={hidden}>
-        <S.ShareOption onClick={kakaoApi}>카카오톡 공유</S.ShareOption>
-        <S.ShareOption onClick={cloneURL}>URL 공유</S.ShareOption>
-      </S.Wrap>
-    </S.ShareBtn>
+    <>
+      <S.ShareBtn onClick={() => setHidden(!hidden)}>
+        <S.ButtonIconImg src="/images/icons/shareIcon.svg" alt="공유버튼 아이콘" />
+        <S.Wrap hidden={hidden}>
+          <KakaoShareButton />
+          <S.ShareOption onClick={handleCopy}>URL 공유</S.ShareOption>
+        </S.Wrap>
+      </S.ShareBtn>
+      {isCopied && <Toast />}
+    </>
   );
 }
 
