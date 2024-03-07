9ee1530694e9c0c1e3b30e854f0cf409069c5764
Jules <dali1999@naver.com>
2024-03-08 03:57:02 +0900

    design: color 뒷배경 추가 및 스켈레톤 ui 배경 색 변경

--- src/components/paperCard/PaperCard.style.js
+++ src/components/paperCard/PaperCard.style.js
@@ -22,6 +22,7 @@ export const PaperCard = styled.div`
   overflow: hidden;
   color: ${(props) => (props.$backgroundImageURL ? 'white' : 'black')};
   cursor: pointer;
+  z-index: -1;
 
   &::before {
     content: '';
@@ -30,12 +31,23 @@ export const PaperCard = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background-image: ${(props) => (props.$backgroundImageURL ? `url(${props.$backgroundImageURL})` : 'none')};
+    background-image: ${(props) => props.$backgroundImageURL && `url(${props.$backgroundImageURL})`};
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
     filter: blur(2px) brightness(0.9);
     z-index: -1;
+  }
+
+  &::after {
+    content: '';
+    background-image: ${(props) => !props.$backgroundImageURL && `url(${props.$pattern})`};
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    width: 142px;
+    height: 142px;
   }
 `;
 
