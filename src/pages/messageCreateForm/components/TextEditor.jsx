c5aa621e9f6f3603dabc90619324798ff08bb7c5
parkhyeonwoo <pq12owal@naver.com>
2024-03-10 00:17:31 +0900

    fix : textEditor toolbar 수정

--- src/pages/messageCreateForm/components/TextEditor.jsx
+++ src/pages/messageCreateForm/components/TextEditor.jsx
@@ -4,10 +4,10 @@ import './TextEditor.css';
 import 'react-quill/dist/quill.snow.css';
 
 const modules = {
-  toolbar: [['bold', 'italic', 'underline'], [{ align: [] }, { color: [] }], ['clean']],
+  toolbar: [['bold', 'italic', 'underline'], [{ color: [] }], ['clean']],
 };
 
-const formats = ['bold', 'italic', 'underline', 'bullet', 'indent', 'align', 'color'];
+const formats = ['bold', 'italic', 'underline', 'bullet', 'indent', 'color'];
 
 function TextEditor({ messageLength, dispatch }) {
   const handleOnChange = (content, delta, source, editor) => {
