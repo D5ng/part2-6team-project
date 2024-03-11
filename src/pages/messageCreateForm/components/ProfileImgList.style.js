2e04cff874cc3b60e816c53443937e8acb5212a2
parkhyeonwoo <pq12owal@naver.com>
2024-03-11 15:47:42 +0900

    Design : 사용자 경험 messageForm페이지 개선

--- src/pages/messageCreateForm/components/ProfileImgList.style.js
+++ src/pages/messageCreateForm/components/ProfileImgList.style.js
@@ -20,6 +20,9 @@ export const ListItem = styled.li`
   border-radius: 100px;
   overflow: hidden;
   cursor: pointer;
+  &:last-child {
+    overflow: visible;
+  }
   @media ${device.mobile} {
     max-width: 60px;
     width: 100%;
@@ -28,6 +31,7 @@ export const ListItem = styled.li`
 export const ProfileImg = styled.img`
   width: 100%;
   transition: 0.5s;
+  -webkit-user-drag: none;
   &:hover {
     transform: scale(1.3);
   }
@@ -39,17 +43,49 @@ export const SearchBtb = styled.button`
   border-radius: 100px;
   background-color: var(--gray300);
   padding: 8px;
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 15px;
+    height: 15px;
+    background-color: var(--primary300);
+    bottom: -35%;
+    left: 50%;
+    transform: translateX(-50%) rotate(45deg);
+    visibility: hidden;
+  }
+  &::after {
+    content: '🔍사진 검색';
+    position: absolute;
+    bottom: -50%;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 80px;
+    padding: 2px 5px;
+    border-radius: 5px;
+    background-color: var(--primary300);
+    visibility: hidden;
+  }
+  &:hover::after,
+  &:hover::before {
+    visibility: visible;
+  }
   @media ${device.mobile} {
     max-width: 60px;
     width: 100%;
     height: 100%;
-    /* aspect-ratio: 1/1; */
+    &::after {
+      display: none;
+    }
   }
 `;
 
 export const SearchImg = styled.img`
   width: 100%;
   transition: 0.5s;
+  -webkit-user-drag: none;
   &:hover {
     transform: scale(1.2);
   }
