cbb99ea3757af94e51ebc81cb1a432205f2aecb0
d5ng <dong5ffice@gmail.com>
2024-03-12 05:02:47 +0900

    Swiper Reset

--- src/pages/list/components/LatestPaper.jsx
+++ src/pages/list/components/LatestPaper.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Navigation, Virtual } from 'swiper/modules';
+import React, { useRef, useState } from 'react';
+import { Navigation } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/virtual';
@@ -9,30 +9,10 @@ import * as S from '@List/components/Paper.style';
 import breakPoints from '@List/utils/swiper.breakpoints';
 import PaperCard from '@Components/paperCard/PaperCard';
 
-let swiperModul = null;
-
 function PopularPaper({ papers }) {
   const [_, setInit] = useState(false);
   const navigationPrevRef = useRef(null);
   const navigationNextRef = useRef(null);
-
-  const handleResize = () => {
-    if (window.innerWidth < 1290) {
-      swiperModul = false;
-    } else {
-      swiperModul = true;
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    window.addEventListener('load', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      window.removeEventListener('load', handleResize);
-    };
-  }, []);
 
   return (
     <S.SwiperWrapper>
@@ -44,8 +24,7 @@ function PopularPaper({ papers }) {
         }}
         breakpoints={breakPoints}
         className="mySwiper"
-        modules={swiperModul ? [(Navigation, Virtual)] : null}
-        virtual={swiperModul ? true : null}
+        modules={[Navigation]}
       >
         {papers.map((list) => (
           <S.StyledSlide key={list.id}>
