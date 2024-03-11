c47b87cb09b10cb0674710c8044625c8dc3a8ec1
Jules <dali1999@naver.com>
2024-03-11 16:20:38 +0900

    feat: '모든 페이지 보기' 버튼 추가

--- src/pages/list/RollingList.style.js
+++ src/pages/list/RollingList.style.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 import { device } from 'styles/breakpoints';
+import { Link } from 'react-router-dom';
+
+export const RollingListContainer = styled.div`
+  margin-bottom: 130px;
+`;
 
 export const Section = styled.section`
   margin-top: 50px;
@@ -14,6 +19,7 @@ export const Section = styled.section`
 `;
 
 export const Wrapper = styled.div`
+  position: relative;
   width: 100%;
   max-width: 1200px;
   margin-left: auto;
@@ -35,10 +41,44 @@ export const Title = styled.h2`
 `;
 
 export const LinkArea = styled.div`
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
   margin-top: 64px;
 
   @media ${device.tablet} {
     padding: 0 24px;
+  }
+`;
+
+export const AllPaperLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+
+  width: 280px;
+  height: 56px;
+  font-size: 18px;
+  border-radius: 12px;
+  border: 1px solid var(--gray300);
+  margin-left: auto;
+  margin-right: auto;
+  z-index: 3;
+  transition: 0.3s cubic-bezier(0.67, 0.07, 0.34, 0.98);
+
+  @media ${device.tablet} {
+    position: fixed;
+    bottom: 93px;
+    left: 20px;
+    width: calc(100% - 40px);
+  }
+  @media ${device.mobile} {
+    position: fixed;
+    bottom: 93px;
+    left: 20px;
+    width: calc(100% - 40px);
   }
 `;
